0cfe4982fb824ac81a15e961d6dc5e3e1bcc0ac7
wilix-lead <lead@w.wilix.ru>
2017-02-20 15:43:59 +0300

    Added simple form validation

--- frontend/src/js/controllers/main-controller.js
+++ frontend/src/js/controllers/main-controller.js
@@ -47,8 +47,18 @@ function mainController(
           retail_cost: 0
         };
         $scope.categories = [];
+        $scope.err = {};
 
         $scope.yes = function() {
+          let hasErrors = false;
+          if (!$scope.product.title) { $scope.err.title = true; hasErrors = true; } else { $scope.err.title = false; }
+          if (!$scope.product.cost) { $scope.err.cost = true; hasErrors = true; } else { $scope.err.cost = false; }
+          if (!$scope.product.retail_cost) { $scope.err.retail_cost = true; hasErrors = true; } else { $scope.err.retail_cost = false; }
+          if (hasErrors) {
+            return;
+          }
+          $scope.err = {};
+          
           productService.addProduct($scope.product).then((newProduct) => {
             $rootScope.$broadcast('productAdded', newProduct);
             $scope.cancel();
--- frontend/src/js/controllers/product-controller.js
+++ frontend/src/js/controllers/product-controller.js
@@ -19,9 +19,20 @@ function productController(
       controller: function($scope, $uibModalInstance, categoryService) {
         $scope.title = 'Изменить продукт';
         $scope.product = angular.copy(product);
+        $scope.product.category = $scope.product.category + ''; 
         $scope.categories = [];
+        $scope.err = {};
         
         $scope.yes = function() {
+          let hasErrors = false;
+          if (!$scope.product.title) { $scope.err.title = true; hasErrors = true; } else { $scope.err.title = false; }
+          if (!$scope.product.cost) { $scope.err.cost = true; hasErrors = true; } else { $scope.err.cost = false; }
+          if (!$scope.product.retail_cost) { $scope.err.retail_cost = true; hasErrors = true; } else { $scope.err.retail_cost = false; }
+          if (hasErrors) {
+            return;
+          }
+          $scope.err = {};
+          
           productService.updateProduct($scope.product).then((newProduct) => {
             if (product.category == newProduct.category) {
               vm.products[vm.products.indexOf(product)] = newProduct;
