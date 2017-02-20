97f1fa5c278ed6239aa261741a72e25597a8b048
wilix-lead <lead@w.wilix.ru>
2017-02-20 15:48:14 +0300

    Update paths in config

--- frontend/build.config.js
+++ frontend/build.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   prod: [
-    ['${API_URL}$', 'https://test.wilix.ru/api'],
-    ['${HTML_BASE_HREF}$', 'https://test.wilix.ru/app/']
+    ['${API_URL}$', 'http://wilix.ru:5687/api'],
+    ['${HTML_BASE_HREF}$', 'https://wilix.ru/test-project/test-product-manager']
   ],
   dev: [
     ['${API_URL}$', 'http://localhost:5687/api'],
